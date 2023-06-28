f90158df76e5ef4fbfe4f4637a502a3f7815545d
Kathryn McKean <KMCKEAN907@GMAIL.COM>
2023-06-27 19:51:38 -0500

    new visual and theme, building out into a magic 8 ball kind of thing

--- app.js
+++ app.js
@@ -1,27 +1,109 @@
-async function getGif() {
-    // let userInput = document.getElementById("input").value
-    let userInput = document.getElementById("input").value
-    console.log(userInput)
+function willThey() {
 
-    let giphyApiKey = process.env.API_KEY
-    let giphyApiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg-13&api_key=${giphyApiKey}`
+    let title = document.createElement("p")
 
-    const response = await fetch(giphyApiURL)
-    console.log(response)
+    title.textContent = "will they..."
 
-    const data = await response.json();
-    console.log(data)
 
-    let num = Math.floor((Math.random() * 25))
-    //random number between 0 and 24  
-    let imgPath = data.data[num].images.fixed_height.url
-    console.log(imgPath)
-    document.getElementById("gif").src = imgPath; // updates the gif to the most current search
+
+    const questionFrame = document.getElementById("questionFrame")
+
+    questionFrame.replaceChild(title, questionFrame.firstElementChild);
+
+
+
+
+
 }
 
-// giphy gives you 25 results automatically so instead of picking the first one every time...
-// I want to pick a random number between 0 and 24 and use that as my index
-// That should give me more variety if someone enters the same word multiple times hoping for a different result
 
-// Just found out that var is old and out-of-date which is the opposite of what I thought
-// also found out that I should be using async/await
+
+let pTwo = document.getElementById("willThey")
+
+pTwo.addEventListener("click", willThey)
+
+
+
+async function getGif() {
+
+    let userInput = document.getElementById("input").value
+
+    // console.log(userInput)
+
+
+
+    let giphyApiKey = 'wL2VhWAS3RK5vRLV4Xur1HwpaF9O3dpp'
+
+    let giphyApiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg-13&api_key=${giphyApiKey}`
+
+
+
+    const response = await fetch(giphyApiURL)
+
+    // console.log(response)
+
+
+
+    const data = await response.json();
+
+    // console.log(data)
+
+
+
+    let num = Math.floor((Math.random() * 25))
+
+    //random number between 0 and 24
+
+    let imgPath = data.data[num].images.fixed_height.url
+
+    // console.log(imgPath)
+
+    document.getElementById("gif").src = imgPath; // updates the gif to the most current search
+
+}
+
+
+
+let showSearchBar = (string, el) => {
+
+
+
+
+
+    const newInput = document.createElement("input")
+
+    newInput.setAttribute("type", "text");
+
+    newInput.setAttribute("id", "input");
+
+
+
+    const newButton = document.createElement("button")
+
+    newButton.setAttribute("onClick", "getGif()");
+
+    newButton.textContent = `search`
+
+
+
+    let search = document.createElement("div")
+
+    search.appendChild(newInput)
+
+    search.appendChild(newButton)
+
+
+
+    const questionFrame = document.getElementById("questionFrame")
+
+    questionFrame.replaceChild(search, questionFrame.lastElementChild);
+
+
+
+
+
+
+
+    // when you click a prompt it should get bigger and show an imput and search button.
+
+}
