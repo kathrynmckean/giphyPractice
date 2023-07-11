bf58ce1f3e6b1390f310c56b0d8f3136e8ed2bdc
Kathryn McKean <KMCKEAN907@GMAIL.COM>
2023-07-10 21:15:02 -0500

    condenses the three prompt functions into one stacked function

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
-function willI() {
-    let note = document.getElementById('willI');
-    console.log(note.textContent);
-
-    let promptChange = document.getElementById('prompt');
-    promptChange.textContent = note.textContent;
-    console.log("success")
-}
-function willThey() {
-    let note = document.getElementById('willThey');
-    console.log(note.textContent);
-
-    let promptChange = document.getElementById('prompt');
-    promptChange.textContent = note.textContent;
-    console.log("success")
-}
-
 function handlePrompt(choice) {
     showSearchBar();
     if (choice === "should I") {
         let note = document.getElementById('shouldI');
         console.log(note.textContent);
-
         let promptChange = document.getElementById('prompt');
         promptChange.textContent = note.textContent;
         console.log("success")
     } else if (choice === "will I") {
-        console.log("will i")
+        let note = document.getElementById('willI');
+        console.log(note.textContent);
+
+        let promptChange = document.getElementById('prompt');
+        promptChange.textContent = note.textContent;
+        console.log("success")
     } else if (choice === "will they") {
-        console.log("will they");
+        let note = document.getElementById('shouldI');
+        console.log(note.textContent);
+
+        let promptChange = document.getElementById('prompt');
+        console.log(promptChange)
+        promptChange.textContent = note.textContent;
+        console.log("success")
     } else {
         // gracefully exit function
         return;
@@ -35,59 +28,34 @@ function handlePrompt(choice) {
 }
 
 
-
-
-// let pTwo = document.getElementById("willThey")
-
-// pTwo.addEventListener("click", willThey)
-
-
-
 async function getGif() {
 
     let userInput = document.getElementById("input").value
-
     // console.log(userInput)
 
-
-
-    let giphyApiKey = 'wL2VhWAS3RK5vRLV4Xur1HwpaF9O3dpp'
+    let giphyApiKey = 'wL2VhWAS3RK5vRLV4Xur1HwpaF9O3dpp' //in a real product this wouldn't be visible! it would be in a hidden ENV file
 
     let giphyApiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg-13&api_key=${giphyApiKey}`
 
 
-
     const response = await fetch(giphyApiURL)
-
     // console.log(response)
 
-
-
     const data = await response.json();
-
     // console.log(data)
 
-
-
-    let num = Math.floor((Math.random() * 25))
-
-    //random number between 0 and 24
+    let num = Math.floor((Math.random() * 25)) //random number between 0 and 24
 
     let imgPath = data.data[num].images.fixed_height.url
-
     // console.log(imgPath)
 
     document.getElementById("gif").src = imgPath; // updates the gif to the most current search
-
+    console.log("success")
 }
 
 
 
 let showSearchBar = (string, el) => {
-
-
-
-
 
     const newInput = document.createElement("input")
 
@@ -101,7 +69,7 @@ let showSearchBar = (string, el) => {
 
     newButton.setAttribute("onClick", "getGif()");
 
-    newButton.textContent = `search`
+    newButton.textContent = `Search`
 
 
 
@@ -111,18 +79,7 @@ let showSearchBar = (string, el) => {
 
     search.appendChild(newButton)
 
-
-
     const questionFrame = document.getElementById("questionFrame")
 
     questionFrame.replaceChild(search, questionFrame.lastElementChild);
-
-
-
-
-
-
-
-    // when you click a prompt it should get bigger and show an imput and search button.
-
 }
